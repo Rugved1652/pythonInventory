00c290d29ebcf30a0c0100abed23034e8aae82ff
Rugved Patel <patelrugved7@gmail.com>
2023-04-27 22:46:17 +0530

    day 3

---
+++ tut3.py
@@ -0,0 +1,16 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print("The number is positive.")
+elif num == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
