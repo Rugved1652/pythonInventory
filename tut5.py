e493693303118eb418a8d3687ce3774307afb005
Rugved Patel <patelrugved7@gmail.com>
2023-05-03 22:56:07 +0530

    copy

--- tut5.py
+++ tut5.py
@@ -6,3 +6,13 @@ print(tup.count(2)) # Output: 2
 
 # Find the index of the first occurrence of the value 3
 print(tup.index(3)) # Output: 2
+
+
+# Define a tuple
+tup = (1, 2, 3, 4, 2)
+
+# Count the number of occurrences of the value 2
+print(tup.count(2)) # Output: 2
+
+# Find the index of the first occurrence of the value 3
+print(tup.index(3)) # Output: 2
