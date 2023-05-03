f71137e0e22021392a4bec373255b6e1c7db54d2
Rugved Patel <patelrugved7@gmail.com>
2023-05-03 23:01:47 +0530

    d

---
+++ u.py
@@ -0,0 +1,21 @@
+# Define a string
+s = "hello world"
+
+# Capitalize the first letter
+print(s.capitalize()) # Output: "Hello world"
+
+# Convert to uppercase
+print(s.upper()) # Output: "HELLO WORLD"
+
+# Convert to lowercase
+print(s.lower()) # Output: "hello world"
+
+# Remove whitespace
+print(s.strip()) # Output: "hello world"
+
+# Split into a list
+print(s.split()) # Output: ["hello", "world"]
+
+# Join a list into a string
+words = ["hello", "world"]
+print("-".join(words)) # Output: "hello-world"
